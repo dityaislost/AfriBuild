2134d517fe5a4943f889a2af98987139c2e368bd
Divyansh200518 <om200518@gmail.com>
2024-04-28 17:02:48 +0530

    last commit

--- contexts/StateContext.jsx
+++ contexts/StateContext.jsx
@@ -10,59 +10,59 @@ export const StateProvider = ({ children }) => {
 	const [cumulativeVotes, setCumulativeVotes] = useState({}) //
 	const [personalVotes, setPersonalVotes] = useState([])
 
-	useEffect(() => {
-		console.log(cumulativeVotes, personalVotes)
-		async function postReq() {
-			var cumulativeVotesDocs = []
-			Object.keys(cumulativeVotes).forEach((cVote) => {
-				cumulativeVotesDocs.push(cumulativeVotes[cVote])
-			})
-			var personalVotesDocs = []
-			personalVotes.forEach((cVote) => {
-				personalVotesDocs.push({ latlng: cVote })
-			})
-			console.log(cumulativeVotesDocs, personalVotesDocs)
-			const response = await fetch("/api/db", {
-				method: "POST",
-				body: JSON.stringify({
-					cumulativeVotes: cumulativeVotesDocs,
-					personalVotes: personalVotesDocs,
-				}),
-				headers: {
-					"Content-Type": "application/json",
-				},
-			})
-			console.log(await response.json())
-		}
-		return () => {
-			if (personalVotes.length > 0) {
-				// postReq()
-			}
-		}
-	}, [personalVotes, cumulativeVotes])
+	// useEffect(() => {
+	// 	console.log(cumulativeVotes, personalVotes)
+	// 	async function postReq() {
+	// 		var cumulativeVotesDocs = []
+	// 		Object.keys(cumulativeVotes).forEach((cVote) => {
+	// 			cumulativeVotesDocs.push(cumulativeVotes[cVote])
+	// 		})
+	// 		var personalVotesDocs = []
+	// 		personalVotes.forEach((cVote) => {
+	// 			personalVotesDocs.push({ latlng: cVote })
+	// 		})
+	// 		console.log(cumulativeVotesDocs, personalVotesDocs)
+	// 		const response = await fetch("/api/db", {
+	// 			method: "POST",
+	// 			body: JSON.stringify({
+	// 				cumulativeVotes: cumulativeVotesDocs,
+	// 				personalVotes: personalVotesDocs,
+	// 			}),
+	// 			headers: {
+	// 				"Content-Type": "application/json",
+	// 			},
+	// 		})
+	// 		console.log(await response.json())
+	// 	}
+	// 	return () => {
+	// 		if (personalVotes.length > 0) {
+	// 			// postReq()
+	// 		}
+	// 	}
+	// }, [personalVotes, cumulativeVotes])
 
-	useEffect(() => {
-		async function getReq() {
-			const response = await fetch("/api/db", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			})
-			var res = await response.json()
+	// useEffect(() => {
+	// 	async function getReq() {
+	// 		const response = await fetch("/api/db", {
+	// 			method: "GET",
+	// 			headers: {
+	// 				"Content-Type": "application/json",
+	// 			},
+	// 		})
+	// 		var res = await response.json()
 
-			res.cumulativeVotesData.forEach((element) => {
-				// var key = JSON.stringify(element.latlng)
-				setCumulativeVotes((prev) => ({
-					...prev,
-					[JSON.stringify(element.latlng)]: element,
-				}))
-			})
-		}
-		return () => {
-			// getReq()
-		}
-	}, [])
+	// 		res.cumulativeVotesData.forEach((element) => {
+	// 			// var key = JSON.stringify(element.latlng)
+	// 			setCumulativeVotes((prev) => ({
+	// 				...prev,
+	// 				[JSON.stringify(element.latlng)]: element,
+	// 			}))
+	// 		})
+	// 	}
+	// 	return () => {
+	// 		// getReq()
+	// 	}
+	// }, [])
 
 	return (
 		<StateContext.Provider
